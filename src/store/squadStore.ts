c5e2b63b29c5a8ade1c95b3328a6547efd3a9827
Leonardo Alves <leonardo1029leonardo1029@gmail.com>
2026-04-16 21:47:19 -0300

    feat: added persist squad data

--- src/store/squadStore.ts
+++ src/store/squadStore.ts
@@ -1,4 +1,6 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 import { Player } from "../types/player";
 
 type SquadStore = {
@@ -7,22 +9,30 @@ type SquadStore = {
   removePlayer: (id: string) => void;
 };
 
-export const useSquadStore = create<SquadStore>((set) => ({
-  squad: [],
-  addPlayer: (player) =>
-    set((state) => {
-      const exists = state.squad.find((p) => p.id === player.id);
+export const useSquadStore = create<SquadStore>()(
+  persist(
+    (set) => ({
+      squad: [],
+      addPlayer: (player) =>
+        set((state) => {
+          const exists = state.squad.find((p) => p.id === player.id);
 
-      if (exists) {
-        return state;
-      }
+          if (exists) {
+            return state;
+          }
 
-      return {
-        squad: [...state.squad, player],
-      };
+          return {
+            squad: [...state.squad, player],
+          };
+        }),
+      removePlayer: (id) =>
+        set((state) => ({
+          squad: state.squad.filter((p) => p.id !== id),
+        })),
     }),
-  removePlayer: (id) =>
-    set((state) => ({
-      squad: state.squad.filter((p) => p.id !== id),
-    })),
-}));
+    {
+      name: "squad-storage",
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
